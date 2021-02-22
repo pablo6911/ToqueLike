0c16998f5a3ae236276aabcced825c7e44df9a42
Fernandez <pablitopak@gmail.com>
2021-02-22 17:07:26 -0500

    Regisyro usuario

--- go.mod
+++ go.mod
@@ -1,7 +1,8 @@
-module github.com/pablo6911/twittergolang
+module github.com/pablo6911/ToqueLike
 
 go 1.13
 
 require (
-
+	go.mongodb.org/mongo-driver v1.4.6 // indirect
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
 )
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main()  {
-	
+import (
+	"log"
+
+	"github.com/pablo6911/ToqueLike/bd"
+	"github.com/pablo6911/ToqueLike/handlers"
+)
+
+func main() {
+	if bd.ChequeoConnection() == 0 {
+		log.Fatal("Sin conexion a la BD")
+		return
+	}
+
+	handlers.Manejadores()
 }
