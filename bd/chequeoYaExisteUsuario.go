1a22ea51487c132bee09b9d1414d81148fb67fac
Fernandez <pablitopak@gmail.com>
2021-03-01 15:50:19 -0500

    Registro usuario

--- bd/chequeoYaExisteUsuario.go
+++ bd/chequeoYaExisteUsuario.go
@@ -13,7 +13,7 @@ func ChequeoYaExisteUsuario(email string) (models.Usuario, bool, string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	db := MongoCN.Database("Toquelike")
+	db := MongoCN.Database("toquelike")
 	col := db.Collection("usuarios")
 
 	condicion := bson.M{"email": "email"}
