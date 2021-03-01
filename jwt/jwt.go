c644460b445170d48c1d3e3e6790b2203d8d69fb
Fernandez <pablitopak@gmail.com>
2021-03-01 14:57:42 -0500

    Proceso JWT Usuario

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -10,7 +10,7 @@ import (
 //GeneroJWT genera el encriptado con JWT
 func GeneroJWT(t models.Usuario) (string, error) {
 
-	miClave := []byte("ToqueLike")
+	miClave := []byte("Toque")
 
 	payload := jwt.MapClaims{
 		"email":            t.Email,
